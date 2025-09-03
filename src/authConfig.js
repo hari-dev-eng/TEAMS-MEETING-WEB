3045d4ecbb7a1ad17a70b20bd0b8326135b00075
HariPrasath <hariprasath.c@conservesolution.com>
2025-09-03 10:38:23 +0530

    redirect uri issue clear

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
   auth: {
     clientId: "e993e6b1-9127-4d76-8873-adff75542ed1",
     authority: "https://login.microsoftonline.com/388b828e-0007-4935-b8e4-d5d5f5671f61",
-    redirectUri: "http://localhost:3000"
+    redirectUri: "https://teams-meeting-web.vercel.app/"
   },
   cache: {
     cacheLocation: "localStorage",
